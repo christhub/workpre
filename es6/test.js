3151d1a3e7115977e34dac0d86f3c3a689a82b23
Christian <christian.lei1@gmail.com>
2015-11-11 15:44:34 -0800

    try to fix the problems of js

--- es6/test.js
+++ es6/test.js
@@ -1,13 +1,14 @@
- // es6 feature: block-scoped "let" declaration
+// es6 feature: block-scoped "let" declaration
+"use strict";
 let sentences = [
 { subject: 'JavaScript', verb: 'is', object: 'great' }, { subject: 'Elephants', verb: 'are', object: 'large' },
 ];
-    // es6 feature: object destructuring
+// es6 feature: object destructuring
 function say( subject, verb, object ) {
 // es6 feature: template strings
   console.log(`${subject} ${verb} ${object}`);
 }
-    // es6 feature: for..of
+// es6 feature: for..of
 for( let s of sentences) {
   say(s);
 }
